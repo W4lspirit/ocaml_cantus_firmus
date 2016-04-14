55fe46731894b2b5742cd0fc71cbd00dabaa4141
Mathis Caristan <mathis.caristan@gmail.com>
2016-04-14 14:48:30 +0200

    Suppression double

--- treesctructure.ml
+++
@@ -1,7 +0,0 @@
-module type TREESTRUCTURE = 
-sig
-	type holy_tree
-	val init_tree : holy_tree
-        val construire_arbre:int list -> holy_tree
-	val parcours_arbre : holy_tree -> int list
-end ;;
